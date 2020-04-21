99685e452d2e8705b4842d44fa088b5efd616643
huangqingfeng <15110160114@163.com>
2020-04-21 17:25:26 +0800

    JpaSpecificationExecutor

--- src/main/java/com/cms/yxf/repository/UserRepository.java
+++ src/main/java/com/cms/yxf/repository/UserRepository.java
@@ -2,13 +2,14 @@ package com.cms.yxf.repository;
 
 import com.cms.yxf.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+public interface UserRepository extends JpaRepository<UserEntity, Integer> , JpaSpecificationExecutor<UserEntity> {
 
     List<UserEntity> findAll();
 
